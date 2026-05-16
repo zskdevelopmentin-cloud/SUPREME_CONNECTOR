4bfba041fba051f38606537f703038755ba6f0fc
unknown <nivznrahman26@gmail.com>
2026-05-17 01:56:02 +0530

    updations

--- supreme-connector-web/src/app/dashboard/page.tsx
+++ supreme-connector-web/src/app/dashboard/page.tsx
@@ -48,8 +48,7 @@ export default async function DashboardOverview() {
           salesInvoices: true,
           purchaseInvoices: true,
           inventoryItems: true,
-          payments: true,
-          syncLogs: true
+          payments: true
         }
       }
     }
